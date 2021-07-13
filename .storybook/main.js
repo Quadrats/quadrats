8e12c46dba83b1561a0593e88dd212f49eb45757
Chia Yu Pai <fantasyatelier@gmail.com>
2021-07-13 23:00:47 +0800

    build(deps): upgrade node dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,9 @@ const STORIES_PATH = path.resolve(ROOT_PATH, 'stories');
 const TS_CONFIG = path.resolve(ROOT_PATH, 'tsconfig.dev.json');
 
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../stories/**/*.stories.@(tsx|mdx)'],
   addons: ['@storybook/addon-knobs', '@storybook/addon-docs/preset', '@storybook/addon-storysource'],
   webpackFinal: config => {
@@ -17,17 +20,17 @@ module.exports = {
         include: [PACKAGES_PATH, STORIES_PATH],
         use: [
           {
-            loader: 'awesome-typescript-loader',
+            loader: 'ts-loader',
             options: {
-              configFileName: TS_CONFIG
+              configFile: TS_CONFIG
             }
           },
-          {
-            loader: 'react-docgen-typescript-loader',
-            options: {
-              tsconfigPath: TS_CONFIG
-            }
-          }
+          // {
+          //   loader: 'react-docgen-typescript-loader',
+          //   options: {
+          //     tsconfigPath: TS_CONFIG
+          //   }
+          // }
         ]
       },
       {
@@ -36,11 +39,12 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader']
       }
     );
-    config.resolve.plugins.push(
+
+    config.resolve.plugins = [
       new TsconfigPathsPlugin({
         configFile: TS_CONFIG,
       })
-    );
+    ];
 
     return config;
   }
