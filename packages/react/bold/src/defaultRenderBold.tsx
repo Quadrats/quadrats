510b169c29cc45954394a5f04cd2465c5229e192
timchen <timchen10001@gmail.com>
2021-11-23 23:52:19 +0800

    feat(bold): append bold variant logic

--- packages/react/bold/src/defaultRenderBold.tsx
+++ packages/react/bold/src/defaultRenderBold.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import { RenderMarkPropsBase } from '@quadrats/react/_internal';
 
-export const defaultRenderBold = ({ children }: RenderMarkPropsBase<boolean>) => <strong>{children}</strong>;
+export const defaultRenderBold = (variant?: string) => (
+  ({ children }: RenderMarkPropsBase<boolean>) => (
+    <strong className={variant ?? ''}>
+      {children}
+    </strong>
+  )
+);
