8eb66edef82e7c8bd19054d29016c698247eb55c
timchen <timchen10001@gmail.com>
2021-11-23 23:53:50 +0800

    feat(italic): append italic variant logic

--- packages/common/italic/src/createItalic.ts
+++ packages/common/italic/src/createItalic.ts
@@ -1,6 +1,8 @@
 import { createToggleMarkCreator } from '@quadrats/common/toggle-mark';
+import { getVariantType } from '@quadrats/core';
 import { ITALIC_TYPE } from './constants';
 
-export const createItalic = createToggleMarkCreator({
-  type: ITALIC_TYPE,
+export const createItalic = (variant?: string) => createToggleMarkCreator({
+  type: getVariantType(ITALIC_TYPE, variant),
+  parentType: ITALIC_TYPE,
 });
--- packages/react/italic/src/createReactItalic.ts
+++ packages/react/italic/src/createReactItalic.ts
@@ -3,7 +3,9 @@ import { createReactToggleMarkCreator } from '@quadrats/react/toggle-mark';
 import { defaultRenderItalic } from './defaultRenderItalic';
 import { ITALIC_HOTKEY } from './constants';
 
-export const createReactItalic = createReactToggleMarkCreator(createItalic, {
-  hotkey: ITALIC_HOTKEY,
-  render: defaultRenderItalic,
-});
+export const createReactItalic = (variant?: string) => createReactToggleMarkCreator(
+  createItalic(variant), {
+    hotkey: ITALIC_HOTKEY,
+    render: defaultRenderItalic(variant),
+  },
+)();
