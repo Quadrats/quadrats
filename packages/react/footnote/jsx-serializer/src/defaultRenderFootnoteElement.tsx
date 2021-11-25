925a7d2150a0d1c0b71228ca4fd7782b756e92e3
BenChen <BenChen@rytass.com>
2021-11-25 20:39:59 +0800

    feat: footnote jsx render

--- packages/react/footnote/jsx-serializer/src/defaultRenderFootnoteElement.tsx
+++ packages/react/footnote/jsx-serializer/src/defaultRenderFootnoteElement.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { JsxSerializeFootnoteElementProps } from './typings';
 
 export const defaultRenderFootnoteElement = ({ children, element }: JsxSerializeFootnoteElementProps) => (
-  <a href="./#">
-    {`${children}[${element.footnote}]`}
-  </a>
+  <>
+    <a href="./#">{children}</a>
+    <sup style={{ color: 'var(--qdr-sup)' }}>
+      {`[${element.index ?? 1}]`}
+    </sup>
+  </>
 );
