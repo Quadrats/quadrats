4e83e84c343deea805faf02d89a80d52b424c252
timchen <timchen10001@gmail.com>
2021-11-28 23:36:24 +0800

    revert(line-break): revert line-break-shift feature

--- packages/common/line-break/src/typings.ts
+++ packages/common/line-break/src/typings.ts
@@ -13,13 +13,8 @@ export interface LineBreakElement extends Element, WithElementType, Text {
   children: [Text];
 }
 
-export enum LineBreakVariant {
-  ENTER = 'enter',
-  SHIFT_ENTER = 'shift-enter',
-}
-
 export interface LineBreak extends WithElementType, Withable {
   getLineBreakNodes(editor: Editor, options?: GetNodesOptions): Generator<NodeEntry<Node>>;
   isSelectionInLineBreak(editor: Editor, options?: GetNodesOptions): boolean;
-  toggleLineBreakNodes(editor: Editor, variant?: LineBreakVariant): void;
+  toggleLineBreakNodes(editor: Editor): void;
 }
--- packages/react/line-break/src/constants.ts
+++ packages/react/line-break/src/constants.ts
@@ -9,16 +9,6 @@
 export const LINE_BREAK_HOTKEY = 'enter';
 
 /**
- * Default hotkey for toggling line-break. (shift + enter)
- *
- * @remarks
- * If the hotkey is `shift+enter`, and so on.
- *
- * @see [is-hotkey]{@link https://www.npmjs.com/package/is-hotkey}
- */
-export const LINE_BREAK_SHIFT_HOTKEY = 'shift+enter';
-
-/**
  * Default hotkey for initial line-break position.
  *
  * @remarks
