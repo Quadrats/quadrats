47da0ea3d67a3c7616dc26e2ec7af7105779b68e
timchen <timchen10001@gmail.com>
2021-11-30 01:45:25 +0800

    fix(line-break-shift): completed lin-break-shift logic

--- packages/react/line-break/src/components/LineBreakIcon.tsx
+++ packages/react/line-break/src/components/LineBreakIcon.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, useMemo } from 'react';
 import { LineBreakEnter, LineBreakShiftEnter } from '@quadrats/icons';
 import { Icon } from '@quadrats/react/components';
 import { ReactLineBreakIconElementProps } from '../typings';
@@ -7,24 +7,48 @@ function LineBreakIcon({
   attributes,
   element,
 }: ReactLineBreakIconElementProps) {
+  const ref = useRef<HTMLSpanElement | null>(null);
+
+  const icon = useMemo(() => {
+    if (element.text === 'shift-enter') return LineBreakShiftEnter;
+
+    return LineBreakEnter;
+  }, [element.text]);
+
+  useEffect(() => {
+    if (element.text !== 'shift-enter') return;
+
+    const paragraphElement = ref.current?.parentElement;
+
+    if (paragraphElement) {
+      paragraphElement.setAttribute(
+        'style',
+        'margin-inline: 0; margin-block: 0;',
+      );
+    }
+  }, [element.text]);
+
   return (
     <span
       {...attributes}
+      ref={ref}
       contentEditable={false}
       style={{
+        marginBlock: 0,
+        marginInline: 0,
         userSelect: 'none',
-        verticalAlign: 'middle',
       }}
     >
       <Icon
         style={{
           width: '24px',
           height: '24px',
-          paddingTop: element.text === 'enter' ? '1px' : '6px',
+          verticalAlign: 'middle',
         }}
-        icon={element.text === 'enter' ? LineBreakEnter : LineBreakShiftEnter}
+        icon={icon}
         color="#C1C1C1"
       />
+      <br />
     </span>
   );
 }
