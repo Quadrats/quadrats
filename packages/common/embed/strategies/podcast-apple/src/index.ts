12e42b0d82f7d3fb35b20619df8fd2f5ca2a8cf5
BenChen <BenChen@rytass.com>
2022-01-28 14:51:40 +0800

    fix: podcast language use same as input language

--- packages/common/embed/strategies/podcast-apple/src/index.ts
+++ packages/common/embed/strategies/podcast-apple/src/index.ts
@@ -4,6 +4,7 @@ export type PodcastAppleEmbedType = 'podcast-apple';
 
 export type PodcastAppleEmbedElementData = {
   embedType: PodcastAppleEmbedType;
+  language?: string;
   contextId: string;
 };
 
@@ -14,19 +15,20 @@ export type PodcastAppleEmbedStrategy = EmbedStrategy<PodcastAppleEmbedElementDa
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export const PodcastAppleEmbedStrategy: PodcastAppleEmbedStrategy = {
   serialize: (embedCode) => {
-    const result = /^https:\/\/embed.podcasts.apple.com\/[\w-]*\/podcast\/[\S]*\/id([\d]*)/i.exec(embedCode)
-      ?? /^https:\/\/embed.podcasts.apple.com\/[\w-]*\/podcast\/id([\d]*)/i.exec(embedCode)
-      ?? /^https:\/\/podcasts.apple.com\/[\w-]*\/podcast\/[\S]*\/id([\d]*)/i.exec(embedCode);
+    const result = /^https:\/\/embed.podcasts.apple.com\/([\w-]*)\/podcast\/[\S]*\/id([\d]*)/i.exec(embedCode)
+      ?? /^https:\/\/embed.podcasts.apple.com\/([\w-]*)\/podcast\/id([\d]*)/i.exec(embedCode)
+      ?? /^https:\/\/podcasts.apple.com\/([\w-]*)\/podcast\/[\S]*\/id([\d]*)/i.exec(embedCode);
 
     if (result) {
-      const [, contextId] = result;
+      const [, language, contextId] = result;
 
       return {
         embedType: 'podcast-apple',
+        language,
         contextId,
       };
     }
   },
-  deserialize: (data) => `https://embed.podcasts.apple.com/us/podcast/id${data.contextId}`,
-  isElementDataValid: (data) => typeof data.contextId === 'string' && !!(data.contextId),
+  deserialize: data => `https://embed.podcasts.apple.com/${data.language ?? 'us'}/podcast/id${data.contextId}`,
+  isElementDataValid: data => typeof data.contextId === 'string' && !!(data.contextId),
 };
