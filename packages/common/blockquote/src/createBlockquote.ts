245420fefb4163e8bd127992b80fa2238756953c
BenChen <BenChen@rytass.com>
2022-02-24 20:43:10 +0800

    feat(common/block-quote): support line-break in quote

--- packages/common/blockquote/src/createBlockquote.ts
+++ packages/common/blockquote/src/createBlockquote.ts
@@ -1,60 +1,50 @@
 import {
-  createParagraphElement,
   Editor,
-  Element,
   isNodesTypeIn,
-  normalizeOnlyInlineOrTextInChildren,
-  QuadratsElement,
-  toggleNodesType,
-  Transforms,
   WithElementType,
+  wrapNodesWithUnhangRange,
+  unwrapNodesByTypes,
 } from '@quadrats/core';
-import { Blockquote } from './typings';
+import { Blockquote, BlockquoteElement } from './typings';
 import { BLOCKQUOTE_TYPE } from './constants';
 
 export type CreateBlockquoteOptions = Partial<WithElementType>;
 
 export function createBlockquote({ type = BLOCKQUOTE_TYPE }: CreateBlockquoteOptions = {}): Blockquote {
+  const unwrapBlockquote: Blockquote['unwrapBlockquote'] = (editor) => {
+    unwrapNodesByTypes(editor, [type]);
+  };
+
+  const wrapBlockquote: Blockquote['wrapBlockquote'] = (editor: Editor) => {
+    const element: BlockquoteElement = {
+      type,
+      children: [],
+    };
+
+    wrapNodesWithUnhangRange(editor, element, { split: true });
+  };
+
+  const isSelectionInBlockquote: Blockquote['isSelectionInBlockquote'] = editor => (
+    isNodesTypeIn(editor, [type], { mode: 'highest' }));
+
   return {
     type,
-    isSelectionInBlockquote: editor => isNodesTypeIn(editor, [type]),
+    unwrapBlockquote,
+    wrapBlockquote,
+    isSelectionInBlockquote,
+
     toggleBlockquote: (editor) => {
-      const actived = isNodesTypeIn(editor, [type]);
+      const actived = isSelectionInBlockquote(editor);
 
-      toggleNodesType(editor, type);
-
-      if (!actived) {
-        if (editor.selection) {
-          // Select to line end
-          const originPoint = Editor.point(editor, editor.selection.focus);
-
-          Transforms.move(editor, { unit: 'line', edge: 'end' });
-          Transforms.collapse(editor, { edge: 'end' });
-
-          // Only last block add paragraph automatically
-          if (!Editor.next(editor)) {
-            Transforms.insertNodes(editor, [createParagraphElement()]);
-          }
-
-          Transforms.select(editor, originPoint);
+      if (editor.selection) {
+        if (actived) {
+          unwrapBlockquote(editor);
+        } else {
+          wrapBlockquote(editor);
         }
       }
     },
     with(editor) {
-      const { normalizeNode } = editor;
-
-      editor.normalizeNode = (entry) => {
-        const [node] = entry;
-
-        if (Element.isElement(node) && (node as QuadratsElement).type === type) {
-          if (normalizeOnlyInlineOrTextInChildren(editor, entry)) {
-            return;
-          }
-        }
-
-        normalizeNode(entry);
-      };
-
       return editor;
     },
   };
--- packages/common/blockquote/src/typings.ts
+++ packages/common/blockquote/src/typings.ts
@@ -8,6 +8,8 @@ import {
 export interface BlockquoteElement extends QuadratsElement, WithElementType {}
 
 export interface Blockquote<T extends Editor = Editor> extends WithElementType, Withable {
+  unwrapBlockquote(editor: T): void;
+  wrapBlockquote(editor: T): void;
   isSelectionInBlockquote(editor: T): boolean;
   toggleBlockquote(editor: T): void;
 }
