a32bbd8807bec442805768ddf486260e9c58f2e4
TingTing <ting@rytass.com>
2025-04-18 10:06:28 +0800

    feat(react/toolbar): toolbar icon with arrow

--- packages/react/toolbar/src/components/ToolbarIcon.tsx
+++ packages/react/toolbar/src/components/ToolbarIcon.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import clsx from 'clsx';
+import { ArrowDown } from '@quadrats/icons';
 import { Icon, IconProps } from '@quadrats/react/components';
 
 export interface ToolbarIconProps extends Omit<IconProps, 'ref'> {
   active?: boolean;
+  withArrow?: boolean;
 }
 
 function ToolbarIcon(props: ToolbarIconProps) {
   const {
-    active, className, onMouseDown, ...rest
+    active, className, onMouseDown, withArrow, ...rest
   } = props;
 
   return (
-    <Icon
-      {...rest}
-      className={clsx('qdr-toolbar__icon', { 'qdr-toolbar__icon--active': active }, className)}
-      onMouseDown={(event) => {
-        event.preventDefault();
-        onMouseDown?.(event);
-      }}
-    />
+    <div className={clsx('qdr-toolbar__icon', { 'qdr-toolbar__icon--active': active }, className)}>
+      <Icon
+        {...rest}
+        onMouseDown={(event) => {
+          event.preventDefault();
+          onMouseDown?.(event);
+        }}
+      />
+      {withArrow && (
+        <Icon icon={ArrowDown} width={12} height={12} />
+      )}
+    </div>
   );
 }
 
