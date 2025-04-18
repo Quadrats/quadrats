bbdc1905e4b2d2c24fcee620ef6f4abf0f20a61b
TingTing <ting@rytass.com>
2025-04-18 14:11:33 +0800

    feat(locales): toolbar locales

--- packages/locales/src/typings.ts
+++ packages/locales/src/typings.ts
@@ -32,5 +32,44 @@ export interface LocaleDefinition {
     video: {
       inputPlaceholder: string;
     };
+    toolbar: {
+      alignCenter: string;
+      alignLeft: string;
+      alignRight: string;
+      bold: string;
+      bulletList: string;
+      dance: string;
+      divider: string;
+      drama: string;
+      embed: string;
+      erase: string;
+      facebook: string;
+      footnote: string;
+      heading1: string;
+      heading2: string;
+      heading3: string;
+      heading4: string;
+      heading5: string;
+      heading6: string;
+      highlight: string;
+      image: string;
+      instagram: string;
+      italic: string;
+      link: string;
+      numberList: string;
+      paragraph: string;
+      podcastApple: string;
+      quote: string;
+      readMore: string;
+      sheetMusic: string;
+      spotify: string;
+      strikethrough: string;
+      twitter: string;
+      underline: string;
+      unlink: string;
+      video: string;
+      vimeo: string;
+      youtube: string;
+    },
   };
 }
