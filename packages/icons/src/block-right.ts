27e9ef2dba626bb206c8bef439c7c16046d24155
TingTing <ting@rytass.com>
2025-06-03 13:43:05 +0800

    feat(icons): more icons

---
+++ packages/icons/src/block-right.ts
@@ -0,0 +1,15 @@
+import { IconDefinition } from './typings';
+
+export const BlockRight: IconDefinition = {
+  name: 'block-right',
+  definition: {
+    svg: {
+      viewBox: '0 0 24 24',
+    },
+    path: {
+      fillRule: 'evenodd',
+      fill: 'currentColor',
+      d: 'M13.1 16.4V7.6H18.9V16.4H13.1ZM11.5 7C11.5 6.44772 11.9477 6 12.5 6H19.5C20.0523 6 20.5 6.44772 20.5 7V17C20.5 17.5523 20.0523 18 19.5 18H12.5C11.9477 18 11.5 17.5523 11.5 17V7ZM3.5 11H9.9V9.39999H3.5V11ZM3.5 14.6H7.7V13H3.5V14.6Z',
+    },
+  },
+};
