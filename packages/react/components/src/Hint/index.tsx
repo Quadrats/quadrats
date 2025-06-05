1278c976e3c95a2207d7cb1380e9d8fb2b1a80f6
TingTing <ting@rytass.com>
2025-06-05 13:58:22 +0800

    feat(react/components): hint styles

--- packages/react/components/src/Hint/index.tsx
+++ packages/react/components/src/Hint/index.tsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import clsx from 'clsx';
+import { Info, Success, Warning, Error } from '@quadrats/icons';
+import Icon from '../Icon';
 
 export interface HintProps {
   children: string;
+  variant?: 'info' | 'success' | 'warning' | 'error';
 }
 
 const Hint = ({
   children,
+  variant = 'info',
 }: HintProps) => {
+  const icon = useMemo(() => {
+    switch (variant) {
+      case 'info':
+        return Info;
+
+      case 'success':
+        return Success;
+
+      case 'warning':
+        return Warning;
+
+      case 'error':
+        return Error;
+
+      default:
+        return Info;
+    }
+  }, [variant]);
+
   return (
     <div
       className={clsx(
         'qdr-hint',
       )}
     >
-      {children}
+      <div
+        className={clsx(
+          'qdr-hint__icon',
+          `qdr-hint__icon--${variant}`,
+        )}
+      >
+        <Icon icon={icon} width={16} height={16} />
+      </div>
+      <div
+        className={clsx(
+          'qdr-hint__children',
+          `qdr-hint__children--${variant}`,
+        )}
+      >
+        {children}
+      </div>
     </div>
   );
 };
