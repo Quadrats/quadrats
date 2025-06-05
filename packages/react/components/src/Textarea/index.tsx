767f71869290e1bb484c8e53a5029ce43f8e86ff
TingTing <ting@rytass.com>
2025-06-05 17:49:43 +0800

    feat(react/components): textarea styles

--- packages/react/components/src/Textarea/index.tsx
+++ packages/react/components/src/Textarea/index.tsx
@@ -3,16 +3,35 @@ import clsx from 'clsx';
 
 export interface TextareaProps {
   placeholder?: string;
+  height?: number;
+  disabled?: boolean;
+  isError?: boolean;
+  maxLength?: number;
 }
 
-const Textarea = () => {
+const Textarea = ({
+  placeholder,
+  height,
+  disabled,
+  isError,
+}: TextareaProps) => {
   return (
     <div
       className={clsx(
         'qdr-textarea',
       )}
     >
-      textarea
+      <textarea
+        style={{ height }}
+        className={clsx(
+          'qdr-textarea__textarea',
+          {
+            'qdr-textarea__textarea--error': isError,
+          },
+        )}
+        placeholder={placeholder}
+        disabled={disabled}
+      />
     </div>
   );
 };
