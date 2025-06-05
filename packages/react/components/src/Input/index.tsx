e44a1d13541361f649869bd8beb5be42d50e135c
TingTing <ting@rytass.com>
2025-06-05 15:20:24 +0800

    feat(react/components): input styles

--- packages/react/components/src/Input/index.tsx
+++ packages/react/components/src/Input/index.tsx
@@ -3,16 +3,33 @@ import clsx from 'clsx';
 
 export interface InputProps {
   placeholder?: string;
+  disabled?: boolean;
+  isError?: boolean;
+  maxLength?: number;
 }
 
-const Input = () => {
+const Input = ({
+  placeholder,
+  disabled,
+  isError,
+}: InputProps) => {
   return (
     <div
       className={clsx(
         'qdr-input',
       )}
     >
-      input
+      <input
+        className={clsx(
+          'qdr-input__input',
+          {
+            'qdr-input__input--error': isError,
+          },
+        )}
+        type="text"
+        placeholder={placeholder}
+        disabled={disabled}
+      />
     </div>
   );
 };
