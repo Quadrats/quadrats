6b15eda73b91a3cf0edbd2ba6f7fea2c195fa1bc
TingTing <ting@rytass.com>
2025-06-06 09:12:34 +0800

    feat(react/components): textarea behavior

--- packages/react/components/src/Textarea/index.tsx
+++ packages/react/components/src/Textarea/index.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import clsx from 'clsx';
 
 export interface TextareaProps {
+  value?: string;
+  onChange?: (value: string) => void;
   placeholder?: string;
   height?: number;
   disabled?: boolean;
   isError?: boolean;
+  hint?: string;
   maxLength?: number;
 }
 
 const Textarea = ({
+  value = '',
+  onChange,
   placeholder,
   height,
   disabled,
   isError,
+  hint,
+  maxLength,
 }: TextareaProps) => {
+  const isLimited = useMemo(
+    () => (maxLength && value && value.length >= maxLength), [maxLength, value],
+  );
+
   return (
     <div
       className={clsx(
@@ -29,9 +40,35 @@ const Textarea = ({
             'qdr-textarea__textarea--error': isError,
           },
         )}
+        value={value}
+        onChange={(e) => {
+          onChange?.(e.target.value);
+        }}
+        maxLength={maxLength}
         placeholder={placeholder}
         disabled={disabled}
       />
+      {(hint || maxLength) && (
+        <div className="qdr-textarea__hint-wrapper">
+          <span className="qdr-textarea__hint">
+            {hint}
+          </span>
+          {maxLength && (
+            <span
+              className={clsx(
+                'qdr-textarea__counter',
+                {
+                  'qdr-textarea__counter--limited': isLimited,
+                  'qdr-textarea__counter--error': isError,
+                  'qdr-textarea__counter--disabled': disabled,
+                },
+              )}
+            >
+              {`${value ? value.length : 0}/${maxLength}`}
+            </span>
+          )}
+        </div>
+      )}
     </div>
   );
 };
