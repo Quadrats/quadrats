585a5ce4fa5e34ddb264f1e851486584f819f76f
TingTing <ting@rytass.com>
2025-06-24 16:16:24 +0800

    refactor(react/image): better positon of inline toolbar

--- packages/react/image/src/components/ImageFigure.tsx
+++ packages/react/image/src/components/ImageFigure.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import clsx from 'clsx';
-import { Transforms } from '@quadrats/core';
-import { ImageFigureElement } from '@quadrats/common/image';
-import { AlignLeft, AlignCenter, AlignRight, Trash } from '@quadrats/icons';
-import { ReactEditor, useSlateStatic, useFocused, useSelected } from '@quadrats/react';
-import { InlineToolbar } from '../../../toolbar/src';
+import { useFocused, useSelected } from '@quadrats/react';
 import { RenderImageFigureElementProps } from '../typings';
 
 function ImageFigure(props: RenderImageFigureElementProps) {
-  const { attributes, children, element, style } = props;
-
-  const editor = useSlateStatic();
-  const path = ReactEditor.findPath(editor, element);
+  const { attributes, children, style } = props;
 
   const focused = useFocused();
   const selected = useSelected();
@@ -25,40 +18,6 @@ function ImageFigure(props: RenderImageFigureElementProps) {
       })}
       style={style}
     >
-      <InlineToolbar
-        className="qdr-image__inline-toolbar"
-        leftIcons={[
-          {
-            icon: AlignLeft,
-            onClick: () => {
-              Transforms.setNodes(editor, { align: 'start' } as ImageFigureElement, { at: path });
-            },
-            active: element.align === 'start' || !element.align,
-          },
-          {
-            icon: AlignCenter,
-            onClick: () => {
-              Transforms.setNodes(editor, { align: 'center' } as ImageFigureElement, { at: path });
-            },
-            active: element.align === 'center',
-          },
-          {
-            icon: AlignRight,
-            onClick: () => {
-              Transforms.setNodes(editor, { align: 'end' } as ImageFigureElement, { at: path });
-            },
-            active: element.align === 'end',
-          },
-        ]}
-        rightIcons={[
-          {
-            icon: Trash,
-            onClick: () => {
-              Transforms.removeNodes(editor, { at: path });
-            },
-          },
-        ]}
-      />
       {children}
     </figure>
   );
