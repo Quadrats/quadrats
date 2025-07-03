21040143e59bb9e413c475de90a513a826876ed3
TingTing <ting@rytass.com>
2025-07-03 16:52:09 +0800

    feat(common/carousel): defined typings and constants

---
+++ packages/common/carousel/src/constants.ts
@@ -0,0 +1,11 @@
+import { CarouselTypes } from './typings';
+
+export const CAROUSEL_TYPE = 'carousel';
+export const CAROUSEL_IMAGES_TYPE = 'carousel-images';
+export const CAROUSEL_CAPTION_TYPE = 'carousel-caption';
+
+export const CAROUSEL_TYPES: CarouselTypes = {
+  carousel: CAROUSEL_TYPE,
+  carousel_images: CAROUSEL_IMAGES_TYPE,
+  carousel_caption: CAROUSEL_CAPTION_TYPE,
+};
---
+++ packages/common/carousel/src/typings.ts
@@ -0,0 +1,29 @@
+import {
+  Editor,
+  Withable,
+  QuadratsElement,
+  WithElementType,
+  Text,
+} from '@quadrats/core';
+
+export type CarouselTypeKey = 'carousel';
+export type CarouselImagesTypeKey = 'carousel_images';
+export type CarouselCaptionTypeKey = 'carousel_caption';
+
+export type CarouselTypes = Record<CarouselTypeKey | CarouselImagesTypeKey | CarouselCaptionTypeKey, string>;
+
+export interface CarouselElement extends QuadratsElement, WithElementType {}
+
+export interface CarouselImagesElement extends QuadratsElement, WithElementType {
+  children: [Text];
+  images: string[];
+  hosting?: string;
+}
+
+export interface CarouselCaptionElement extends QuadratsElement, WithElementType {}
+
+export interface Carousel<T extends Editor = Editor> extends Withable {
+  types: CarouselTypes;
+  insertCarouselPlaceholder(editor: T): void;
+  removeCarouselPlaceholder(editor: T): void;
+}
