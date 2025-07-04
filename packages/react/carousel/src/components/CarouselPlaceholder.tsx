ed16427604cdbbbd603a305c3e378da58a44ec7e
TingTing <ting@rytass.com>
2025-07-04 13:59:24 +0800

    feat(react/carousel): carousel placeholder layout

--- packages/react/carousel/src/components/CarouselPlaceholder.tsx
+++ packages/react/carousel/src/components/CarouselPlaceholder.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { Icon } from '@quadrats/react/components';
 import { RenderElementProps } from '@quadrats/react';
+import { Image } from '@quadrats/icons';
 
 export function CarouselPlaceholder({
   attributes,
-  children,
 }: {
   attributes?: RenderElementProps['attributes'];
   children: RenderElementProps['children'];
   element: RenderElementProps['element'];
 }) {
-  console.log('first', attributes, children);
-
-  return <div>CarouselPlaceholder</div>;
+  return (
+    <div {...attributes} className="qdr-carousel__placeholder" contentEditable={false}>
+      <div className="qdr-carousel__placeholder__icon-wrapper">
+        <Icon className="qdr-carousel__placeholder__icon" icon={Image} width={48} height={48} />
+      </div>
+      <div className="qdr-carousel__dots">
+        <div className="qdr-carousel__dot qdr-carousel__dot--active" />
+        <div className="qdr-carousel__dot" />
+        <div className="qdr-carousel__dot" />
+        <div className="qdr-carousel__dot" />
+        <div className="qdr-carousel__dot" />
+      </div>
+    </div>
+  );
 }
 
 export default CarouselPlaceholder;
