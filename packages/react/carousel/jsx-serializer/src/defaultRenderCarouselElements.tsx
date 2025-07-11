a77f238b29bb750daccfcbe773f3f2573d137269
TingTing <ting@rytass.com>
2025-07-11 14:12:44 +0800

    feat(react/carousel): remove carousel

--- packages/react/carousel/jsx-serializer/src/defaultRenderCarouselElements.tsx
+++ packages/react/carousel/jsx-serializer/src/defaultRenderCarouselElements.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Carousel, CarouselImages, CarouselCaption } from '@quadrats/react/carousel';
+import { CarouselWithoutToolbar, CarouselImages, CarouselCaption } from '@quadrats/react/carousel';
 import { CarouselJsxSerializeElements } from './typings';
 
 export const defaultRenderCarouselElements: CarouselJsxSerializeElements = {
-  carousel: (props) => <Carousel {...props} />,
+  carousel: (props) => <CarouselWithoutToolbar {...props} />,
   carousel_images: (props) => <CarouselImages {...props} />,
   carousel_caption: (props) => <CarouselCaption {...props} />,
 };
--- packages/react/carousel/src/components/Carousel.tsx
+++ packages/react/carousel/src/components/Carousel.tsx
@@ -1,9 +1,57 @@
 import React, { useState } from 'react';
-import { RenderElementProps } from '@quadrats/react';
+import { Transforms } from '@quadrats/core';
+import { RenderElementProps, useSlateStatic, ReactEditor } from '@quadrats/react';
+import { InlineToolbar } from '@quadrats/react/toolbar';
+import { Edit, Trash } from '@quadrats/icons';
 import { CarouselContext } from '../contexts/CarouselContext';
 import { RenderCarouselElementProps } from '../typings';
 
 export function Carousel({
+  attributes,
+  children,
+  element,
+}: {
+  attributes?: RenderElementProps['attributes'];
+  children: RenderElementProps['children'];
+  element: RenderCarouselElementProps['element'];
+}) {
+  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const editor = useSlateStatic();
+  const path = ReactEditor.findPath(editor, element);
+
+  return (
+    <CarouselContext.Provider
+      value={{
+        activeIndex,
+        setActiveIndex,
+      }}
+    >
+      <div {...attributes} contentEditable={false} className="qdr-carousel">
+        <InlineToolbar
+          className="qdr-carousel__inline-toolbar"
+          leftIcons={[]}
+          rightIcons={[
+            {
+              icon: Edit,
+              onClick: () => {
+                //
+              },
+            },
+            {
+              icon: Trash,
+              onClick: () => {
+                Transforms.removeNodes(editor, { at: path });
+              },
+            },
+          ]}
+        />
+        {children}
+      </div>
+    </CarouselContext.Provider>
+  );
+}
+
+export function CarouselWithoutToolbar({
   attributes,
   children,
 }: {
@@ -20,7 +68,7 @@ export function Carousel({
         setActiveIndex,
       }}
     >
-      <div {...attributes} contentEditable={false}>
+      <div {...attributes} contentEditable={false} className="qdr-carousel">
         {children}
       </div>
     </CarouselContext.Provider>
