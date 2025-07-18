4f2f10efdabd87b68f327786dab6c5596ad13e6f
TingTing <ting@rytass.com>
2025-07-18 15:38:14 +0800

    feat(react/components): create Message component

---
+++ packages/react/components/src/Message/index.tsx
@@ -0,0 +1,115 @@
+import React, { FC, Key, useEffect, useState } from 'react';
+import clsx from 'clsx';
+import Icon from '../Icon';
+import { Info, Success, Warning, Error, IconDefinition } from '@quadrats/icons';
+import { createNotifier } from '../Notifier/createNotifier';
+import { Notifier, NotifierData, NotifierConfig } from '../Notifier/typings';
+
+export type MessageSeverity = 'success' | 'warning' | 'error' | 'info';
+
+export interface MessageConfigProps extends Pick<NotifierConfig, 'duration'> {}
+
+export interface MessageData extends Omit<NotifierData, 'onClose'>, MessageConfigProps {
+  /**
+   * If given, the message will be closed after the amount of time.
+   * You can use `Message.config` to overwrite.
+   * @default 3000
+   */
+  duration?: NotifierData['duration'];
+  /**
+   * message icon prefix
+   */
+  icon?: IconDefinition;
+  /**
+   * The key of message.
+   */
+  reference?: Key;
+  /**
+   * The severity of the message.
+   */
+  severity?: MessageSeverity;
+}
+
+export type MessageType = FC<MessageData> &
+  Notifier<MessageData, MessageConfigProps> &
+  Record<
+    string,
+    (message: MessageData['children'], props?: Omit<MessageData, 'children' | 'severity' | 'icon'>) => Key
+  >;
+
+const Message: MessageType = ((props) => {
+  const { children, duration, icon, severity } = props;
+
+  const [open, setOpen] = useState(true);
+
+  useEffect(() => {
+    if (open && duration) {
+      const timer = window.setTimeout(() => {
+        setOpen(false);
+      }, duration);
+
+      return () => {
+        window.clearTimeout(timer);
+      };
+    }
+  }, [open, duration]);
+
+  return (
+    <div
+      className={clsx('qdr-message', {
+        [`qdr-message--${severity}`]: !!severity,
+      })}
+    >
+      {icon ? <Icon className="qdr-message__icon" icon={icon} /> : null}
+      <span className="qdr-message__content">{children}</span>
+    </div>
+  );
+}) as MessageType;
+
+const { add, config, destroy, remove } = createNotifier<MessageData, MessageConfigProps>({
+  duration: 3000,
+  render: (message) => <Message {...message} key={undefined} />,
+  setRoot: (root) => {
+    root?.setAttribute('class', 'qdr-message__root');
+  },
+});
+
+Message.add = add;
+Message.config = config;
+Message.destroy = destroy;
+Message.remove = remove;
+
+const severities = [
+  {
+    key: 'success',
+    icon: Success,
+  },
+  {
+    key: 'warning',
+    icon: Warning,
+  },
+  {
+    key: 'error',
+    icon: Error,
+  },
+  {
+    key: 'info',
+    icon: Info,
+  },
+];
+
+const validSeverities: MessageSeverity[] = ['success', 'warning', 'error', 'info'];
+
+severities.forEach((severity) => {
+  Message[severity.key] = (message, props) =>
+    Message.add({
+      ...props,
+      children: message,
+      severity: validSeverities.includes(severity.key as MessageSeverity)
+        ? (severity.key as MessageSeverity)
+        : undefined,
+      icon: severity.icon,
+    });
+});
+
+export default Message;
---
+++ packages/react/components/src/Notifier/createNotifier.tsx
@@ -0,0 +1,100 @@
+import React, { createRef } from 'react';
+import { createRoot, Root } from 'react-dom/client';
+import { Notifier, NotifierConfig, NotifierData, RenderNotifier } from './typings';
+import NotifierManager, { NotifierController } from './NotifierManager';
+
+export interface CreateNotifierProps<N extends NotifierData, C extends NotifierConfig> extends NotifierConfig {
+  /**
+   * Customizable config for notifier. The given values should be retrivable from your notification carrier.
+   */
+  config?: C;
+  /**
+   * The render props for notification carrier(UI component).
+   */
+  render: RenderNotifier<N>;
+  /**
+   * The method to set attributes or listeners to root.
+   */
+  setRoot?: (root: HTMLDivElement) => void;
+}
+
+/**
+ * The utility factory for `mezzanine` to create a notifier.
+ *
+ * When APIs are called, Notifier will dynamically render a new react instance by `ReactDOM.render` method.
+ */
+export function createNotifier<N extends NotifierData, C extends NotifierConfig = NotifierConfig>(
+  props: CreateNotifierProps<N, C>,
+): Notifier<N, C> {
+  const { config: configProp, render: renderNotifier, setRoot, duration, maxCount, ...restNotifierProps } = props;
+  const container = typeof document === 'undefined' ? null : document.createElement('div');
+  const root: Root | null = container ? createRoot(container) : null;
+  const controllerRef = createRef<NotifierController<N>>();
+  let currentConfig = {
+    duration,
+    maxCount,
+    ...configProp,
+  };
+
+  if (setRoot && container) {
+    setRoot(container);
+  }
+
+  return {
+    add(notifier) {
+      if (container === null) return 'NOT_SET';
+
+      document.body.appendChild(container as HTMLDivElement);
+
+      const key = notifier.key ?? Date.now();
+
+      const resolvedNotifier = {
+        ...restNotifierProps,
+        ...notifier,
+        ...currentConfig,
+        duration: notifier.duration ?? currentConfig.duration,
+        key,
+        instanceKey: key,
+      };
+
+      if (controllerRef.current) {
+        controllerRef.current.add(resolvedNotifier);
+      } else {
+        root?.render(
+          <NotifierManager<N>
+            controllerRef={controllerRef}
+            defaultNotifiers={[resolvedNotifier]}
+            maxCount={currentConfig.maxCount}
+            render={renderNotifier}
+          />,
+        );
+      }
+
+      return resolvedNotifier.key;
+    },
+    remove(key) {
+      if (controllerRef.current) {
+        controllerRef.current.remove(key);
+      }
+    },
+    destroy() {
+      if (container === null) return;
+
+      // when useEffect(() => () => { root.unmount() }, []), will be show "Rendered more hooks than during the previous render." issue.
+      root?.render(null);
+
+      if (container.parentNode) {
+        container.parentNode.removeChild(container);
+      }
+    },
+    config(config) {
+      currentConfig = {
+        ...currentConfig,
+        ...config,
+      };
+    },
+    getConfig() {
+      return currentConfig as C;
+    },
+  };
+}
