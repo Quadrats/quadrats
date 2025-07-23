794a31f9d7c3f9ce0fad620b28df8580500c2d56
TingTing <ting@rytass.com>
2025-07-23 16:06:09 +0800

    feat(react/card): card typings

--- packages/react/card/src/typings.ts
+++ packages/react/card/src/typings.ts
@@ -1,11 +1,42 @@
+import { Dispatch, SetStateAction } from 'react';
 import {
+  Card,
+  CardElement,
+  CardImageElement,
+  CardContentsElement,
   CardPlaceholderElement,
-  // CardTypeKey,
-  // CardImageTypeKey,
-  // CardContentsTypeKey,
+  CardTypeKey,
+  CardImageTypeKey,
+  CardContentsTypeKey,
 } from '@quadrats/common/card';
-import { RenderElementProps } from '@quadrats/react';
+import { RenderElementProps, Editor, WithCreateRenderElement, Handlers, ConfirmModalConfig } from '@quadrats/react';
+
+export interface RenderCardElementProps extends RenderElementProps<CardElement> {
+  controller: Card<Editor>;
+}
+
+export interface RenderCardImageElementProps extends RenderElementProps<CardImageElement> {}
+
+export interface RenderCardContentsElementProps extends RenderElementProps<CardContentsElement> {}
 
 export interface RenderCardPlaceholderElementProps extends RenderElementProps<CardPlaceholderElement> {}
 
 export type RenderCardPlaceholderElement = (props: RenderCardPlaceholderElementProps) => JSX.Element | null | undefined;
+
+export type CardRenderElements = Record<
+  CardTypeKey,
+  (props: RenderCardElementProps) => JSX.Element | null | undefined
+> &
+  Record<CardImageTypeKey, (props: RenderCardImageElementProps) => JSX.Element | null | undefined> &
+  Record<CardContentsTypeKey, (props: RenderCardContentsElementProps) => JSX.Element | null | undefined>;
+
+export type ReactCardCreateRenderElementOptions = {
+  [K in CardTypeKey | CardImageTypeKey | CardContentsTypeKey]?: CardRenderElements[K];
+};
+
+export interface ReactCard extends Card<Editor>, WithCreateRenderElement<[ReactCardCreateRenderElementOptions?]> {
+  createHandlers: (setNeedConfirmModal?: Dispatch<SetStateAction<ConfirmModalConfig | null>>) => Handlers;
+  createRenderPlaceholderElement: (params_0?: {
+    render?: RenderCardPlaceholderElement;
+  }) => (props: RenderElementProps) => JSX.Element | null | undefined;
+}
