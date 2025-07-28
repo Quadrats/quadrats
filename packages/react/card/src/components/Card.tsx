9fb6741e4dc5b8585bc8bf02d62032568122e692
TingTing <ting@rytass.com>
2025-07-28 15:36:03 +0800

    feat(react/card): card inline toolbar actions

--- packages/react/card/src/components/Card.tsx
+++ packages/react/card/src/components/Card.tsx
@@ -1,34 +1,112 @@
 import React from 'react';
 import clsx from 'clsx';
-import { RenderElementProps } from '@quadrats/react';
+import { Transforms } from '@quadrats/core';
+import { RenderElementProps, useSlateStatic, ReactEditor, useModal } from '@quadrats/react';
 import { InlineToolbar } from '@quadrats/react/toolbar';
-import { Edit, Trash } from '@quadrats/icons';
+import { BlockLeft, BlockRight, Edit, Trash } from '@quadrats/icons';
 import { RenderCardElementProps } from '../typings';
 
 export function Card({
   attributes,
   children,
   element,
-  // controller,
+  controller,
 }: {
   attributes?: RenderElementProps['attributes'];
   children: RenderElementProps['children'];
   element: RenderCardElementProps['element'];
   controller: RenderCardElementProps['controller'];
 }) {
+  const { setCardModalConfig, setConfirmModalConfig } = useModal();
+  const editor = useSlateStatic();
+  const path = ReactEditor.findPath(editor, element);
+
   return (
     <div {...attributes} contentEditable={false} className={clsx('qdr-card', `qdr-card--${element.alignment}`)}>
       <InlineToolbar
         className="qdr-card__inline-toolbar"
-        leftIcons={[]}
+        leftIcons={
+          element.alignment === 'noImage'
+            ? []
+            : [
+                {
+                  icon: BlockLeft,
+                  onClick: () => {
+                    controller.updateCardAlignment({
+                      editor,
+                      alignment: 'leftImageRightText',
+                      path,
+                    });
+                  },
+                  active: element.alignment === 'leftImageRightText',
+                },
+                {
+                  icon: BlockRight,
+                  onClick: () => {
+                    controller.updateCardAlignment({
+                      editor,
+                      alignment: 'rightImageLeftText',
+                      path,
+                    });
+                  },
+                  active: element.alignment === 'rightImageLeftText',
+                },
+              ]
+        }
         rightIcons={[
           {
             icon: Edit,
-            onClick: () => {},
+            onClick: () => {
+              setCardModalConfig({
+                controller,
+                initialValue: {
+                  values: {
+                    alignment: element.alignment,
+                    title: element.title,
+                    description: element.description,
+                    remark: element.remark,
+                    linkText: element.linkText,
+                    linkUrl: element.linkUrl,
+                  },
+                  imageItem: element.imageItem,
+                  haveLink: element.haveLink,
+                },
+                onConfirm: ({ values, imageItem, haveLink }) => {
+                  controller.updateCardElement({
+                    editor,
+                    cardValues: {
+                      alignment: values.alignment,
+                      imageItem,
+                      title: values.title,
+                      description: values.description,
+                      remark: values.remark,
+                      haveLink,
+                      linkText: values.linkText,
+                      linkUrl: values.linkUrl,
+                    },
+                    path,
+                  });
+                },
+              });
+            },
           },
           {
             icon: Trash,
-            onClick: () => {},
+            onClick: () => {
+              if (controller.confirmModal) {
+                // TODO: i18n
+                setConfirmModalConfig({
+                  title: '刪除卡片',
+                  content: '是否確認刪除此卡片？刪除後將立即移除，且此操作無法復原。',
+                  confirmText: '刪除卡片',
+                  onConfirm: () => {
+                    Transforms.removeNodes(editor, { at: path });
+                  },
+                });
+              } else {
+                Transforms.removeNodes(editor, { at: path });
+              }
+            },
           },
         ]}
       />
