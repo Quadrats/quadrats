a44e58a82f76166ddfa1dc1ff61949cb8942b1cc
TingTing <ting@rytass.com>
2025-07-28 15:12:01 +0800

    feat(icons): add chevron-right icon

---
+++ packages/icons/src/chevron-right.ts
@@ -0,0 +1,14 @@
+import { IconDefinition } from './typings';
+
+export const ChevronRight: IconDefinition = {
+  name: 'chevron-right',
+  definition: {
+    svg: {
+      viewBox: '0 0 24 24',
+    },
+    path: {
+      fill: 'currentColor',
+      d: 'M16 12.0005L8.9799 5L8 5.97617V5.97815L14.0412 12.0005L8 18.0238V18.0258L8.9789 19H8.98089L16 12.0005Z',
+    },
+  },
+};
