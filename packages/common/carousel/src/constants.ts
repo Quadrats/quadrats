d01eb231f5b97c08ae6e31dae1bc68d811469b41
TingTing <ting@rytass.com>
2025-07-30 17:52:42 +0800

    fix(common/carousel): default types constants

--- packages/common/carousel/src/constants.ts
+++ packages/common/carousel/src/constants.ts
@@ -1,9 +1,9 @@
 import { CarouselTypes } from './typings';
 
 export const CAROUSEL_TYPE = 'carousel';
-export const CAROUSEL_IMAGES_TYPE = 'carousel-images';
-export const CAROUSEL_CAPTION_TYPE = 'carousel-caption';
-export const CAROUSEL_PLACEHOLDER_TYPE = 'carousel-placeholder';
+export const CAROUSEL_IMAGES_TYPE = 'carousel_images';
+export const CAROUSEL_CAPTION_TYPE = 'carousel_caption';
+export const CAROUSEL_PLACEHOLDER_TYPE = 'carousel_placeholder';
 
 export const CAROUSEL_TYPES: CarouselTypes = {
   carousel: CAROUSEL_TYPE,
