5ac55d9fbe671ed50b60da3a80039f2cdc9963a4
TingTing <ting@rytass.com>
2025-07-30 17:51:53 +0800

    fix(common/card): default types constants

--- packages/common/card/src/constants.ts
+++ packages/common/card/src/constants.ts
@@ -1,9 +1,9 @@
 import { CardTypes } from './typings';
 
 export const CARD_TYPE = 'card';
-export const CARD_IMAGE_TYPE = 'card-image';
-export const CARD_CONTENTS_TYPE = 'card-contents';
-export const CARD_PLACEHOLDER_TYPE = 'card-placeholder';
+export const CARD_IMAGE_TYPE = 'card_image';
+export const CARD_CONTENTS_TYPE = 'card_contents';
+export const CARD_PLACEHOLDER_TYPE = 'card_placeholder';
 
 export const CARD_TYPES: CardTypes = {
   card: CARD_TYPE,
