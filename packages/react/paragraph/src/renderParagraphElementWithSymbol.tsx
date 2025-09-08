1ef74800f359bde5b244b7386e3e8ccaead024e0
travor20814 <travor20814@gmail.com>
2025-09-08 14:49:37 +0800

    feat(react/paragraph): apply align style when receive align props in paragraph

--- packages/react/paragraph/src/renderParagraphElementWithSymbol.tsx
+++ packages/react/paragraph/src/renderParagraphElementWithSymbol.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { ParagraphElement } from '@quadrats/common/paragraph';
 import { RenderElementProps } from '@quadrats/react';
 
 export const renderParagraphElementWithSymbol = ({
   attributes,
   children,
+  element,
 }: {
   attributes?: RenderElementProps['attributes'];
   children: any;
-}) => (
-  <p {...attributes} className="qdr-paragraph qdr-paragraph__with-line-break-symbol">
-    {children}
-  </p>
-);
+  element: ParagraphElement;
+}) => {
+  const style = element.align ? { textAlign: element.align } : undefined;
+
+  return (
+    <p {...attributes} className="qdr-paragraph qdr-paragraph__with-line-break-symbol" style={style}>
+      {children}
+    </p>
+  );
+};
