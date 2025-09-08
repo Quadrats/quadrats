669faef45d04e2bea41617ebba53aaa673f6fc2e
travor20814 <travor20814@gmail.com>
2025-09-08 14:44:54 +0800

    refactor(common/align): simplify types definition

--- packages/common/align/src/createAlign.ts
+++ packages/common/align/src/createAlign.ts
@@ -3,8 +3,9 @@ import { ALIGN_TYPE } from './constants';
 import { AlignValue } from './typings';
 import { ParagraphElement } from '@quadrats/common/paragraph';
 import { HEADING_TYPE, HeadingElement } from '@quadrats/common/heading';
+import { QuadratsReactEditor } from '@quadrats/react';
 
-export interface Align<E extends Editor = Editor> {
+export interface Align<E extends Editor = QuadratsReactEditor> {
   type: string;
   isAlignActive: (editor: E, value: AlignValue) => boolean;
   setAlign: (editor: E, value: AlignValue) => void;
@@ -13,41 +14,35 @@ export interface Align<E extends Editor = Editor> {
 
 const ALIGNABLE_TYPES = [PARAGRAPH_TYPE, HEADING_TYPE];
 
-export function createAlign<E extends Editor = Editor>(): Align<E> {
+export function createAlign(): Align {
   const type = ALIGN_TYPE;
-
-  const isAlignActive = (editor: E, value: AlignValue): boolean => {
-    const { selection } = editor;
-
-    if (!selection) return false;
-
-    const [match] = Editor.nodes(editor, {
-      match: (n) => !!(Element.isElement(n) && n.type && ALIGNABLE_TYPES.includes(n.type)),
-    });
-
-    if (!match) return false;
-
-    const [node] = match;
-
-    return (node as ParagraphElement | HeadingElement)[type] === value;
-  };
-
-  const setAlign = (editor: E, value: AlignValue): void => {
-    Transforms.setNodes(editor, { [type]: value } as ParagraphElement | HeadingElement, {
-      match: (n) => !!(Element.isElement(n) && n.type && ALIGNABLE_TYPES.includes(n.type)),
-    });
-  };
-
-  const removeAlign = (editor: E): void => {
-    Transforms.unsetNodes(editor, [type], {
-      match: (n) => !!(Element.isElement(n) && n.type && ALIGNABLE_TYPES.includes(n.type)),
-    });
-  };
 
   return {
     type,
-    isAlignActive,
-    setAlign,
-    removeAlign,
+    isAlignActive: (editor, value) => {
+      const { selection } = editor;
+
+      if (!selection) return false;
+
+      const [match] = Editor.nodes(editor, {
+        match: (n) => !!(Element.isElement(n) && n.type && ALIGNABLE_TYPES.includes(n.type)),
+      });
+
+      if (!match) return false;
+
+      const [node] = match;
+
+      return (node as ParagraphElement | HeadingElement)[type] === value;
+    },
+    setAlign: (editor, value) => {
+      Transforms.setNodes(editor, { [type]: value } as ParagraphElement | HeadingElement, {
+        match: (n) => !!(Element.isElement(n) && n.type && ALIGNABLE_TYPES.includes(n.type)),
+      });
+    },
+    removeAlign: (editor) => {
+      Transforms.unsetNodes(editor, [type], {
+        match: (n) => !!(Element.isElement(n) && n.type && ALIGNABLE_TYPES.includes(n.type)),
+      });
+    },
   };
 }
