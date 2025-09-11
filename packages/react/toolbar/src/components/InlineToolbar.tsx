4e5d7a6a0fbf241d5939eb41b3e44c89c5d202be
travor20814 <travor20814@gmail.com>
2025-09-11 13:35:38 +0800

    refactor(react/toolbar): inline toolbar refactor to allow multiple iconGroups

--- packages/react/toolbar/src/components/InlineToolbar.tsx
+++ packages/react/toolbar/src/components/InlineToolbar.tsx
@@ -1,65 +1,55 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import clsx from 'clsx';
 import { Icon } from '@quadrats/react/components';
 import { IconDefinition } from '@quadrats/icons';
 
 export interface InlineToolbarProps {
   className?: string;
-  leftIcons: {
-    icon: IconDefinition;
-    onClick: VoidFunction;
-    active: boolean;
-    disabled?: boolean;
-  }[];
-  rightIcons: {
-    icon: IconDefinition;
-    onClick: VoidFunction;
-    disabled?: boolean;
+  iconGroups: {
+    enabledBgColor?: boolean;
+    icons: {
+      icon: IconDefinition;
+      onClick: VoidFunction;
+      active?: boolean;
+      disabled?: boolean;
+      className?: string;
+    }[];
   }[];
 }
 
-function InlineToolbar({ className, leftIcons, rightIcons }: InlineToolbarProps) {
+function InlineToolbar({ className, iconGroups }: InlineToolbarProps) {
+  const validIconsGroup = iconGroups.filter((group) => group.icons.length);
+
   return (
     <div contentEditable={false} className={clsx('qdr-inline-toolbar', className)}>
-      {leftIcons.length > 0 && (
-        <div className="qdr-inline-toolbar__wrapper">
-          {leftIcons.map((icon) => (
-            <Icon
-              key={icon.icon.name}
-              className={clsx('qdr-inline-toolbar__icon', {
-                'qdr-inline-toolbar__icon--active': icon.active,
-                'qdr-inline-toolbar__icon--disabled': icon.disabled,
-              })}
-              icon={icon.icon}
-              width={24}
-              height={24}
-              onClick={(e) => {
-                if (!icon.disabled) {
-                  e.preventDefault();
-                  icon.onClick();
-                }
-              }}
-            />
-          ))}
-        </div>
-      )}
-      {leftIcons.length > 0 && rightIcons.length > 0 && <div className="qdr-inline-toolbar__divider" />}
-      {rightIcons.map((icon) => (
-        <Icon
-          key={icon.icon.name}
-          className={clsx('qdr-inline-toolbar__icon', {
-            'qdr-inline-toolbar__icon--disabled': icon.disabled,
-          })}
-          icon={icon.icon}
-          width={24}
-          height={24}
-          onClick={(e) => {
-            if (!icon.disabled) {
-              e.preventDefault();
-              icon.onClick();
-            }
-          }}
-        />
+      {validIconsGroup.map((group, index) => (
+        <Fragment key={index}>
+          <div
+            className={clsx('qdr-inline-toolbar__wrapper', {
+              'qdr-inline-toolbar__wrapper--enabledBgColor': group.enabledBgColor,
+            })}
+          >
+            {group.icons.map((icon) => (
+              <Icon
+                key={icon.icon.name}
+                className={clsx('qdr-inline-toolbar__icon', icon.className, {
+                  'qdr-inline-toolbar__icon--active': icon.active,
+                  'qdr-inline-toolbar__icon--disabled': icon.disabled,
+                })}
+                icon={icon.icon}
+                width={24}
+                height={24}
+                onClick={(e) => {
+                  if (!icon.disabled) {
+                    e.preventDefault();
+                    icon.onClick();
+                  }
+                }}
+              />
+            ))}
+          </div>
+          {index < validIconsGroup.length - 1 && <div className="qdr-inline-toolbar__divider" />}
+        </Fragment>
       ))}
     </div>
   );
