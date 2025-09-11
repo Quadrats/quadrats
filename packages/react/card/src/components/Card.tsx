c8d1002f743203948782f09dd3433c437398e7c2
travor20814 <travor20814@gmail.com>
2025-09-11 13:36:19 +0800

    refactor(react/card): card inline toolbar refactor due to props changed

--- packages/react/card/src/components/Card.tsx
+++ packages/react/card/src/components/Card.tsx
@@ -25,88 +25,93 @@ export function Card({
     <div {...attributes} contentEditable={false} className={clsx('qdr-card', `qdr-card--${element.alignment}`)}>
       <InlineToolbar
         className="qdr-card__inline-toolbar"
-        leftIcons={
+        iconGroups={[
           element.alignment === 'noImage'
-            ? []
-            : [
-                {
-                  icon: BlockLeft,
-                  onClick: () => {
-                    controller.updateCardAlignment({
-                      editor,
-                      alignment: 'leftImageRightText',
-                      path,
-                    });
-                  },
-                  active: element.alignment === 'leftImageRightText',
-                },
-                {
-                  icon: BlockRight,
-                  onClick: () => {
-                    controller.updateCardAlignment({
-                      editor,
-                      alignment: 'rightImageLeftText',
-                      path,
-                    });
-                  },
-                  active: element.alignment === 'rightImageLeftText',
-                },
-              ]
-        }
-        rightIcons={[
-          {
-            icon: Edit,
-            onClick: () => {
-              setCardModalConfig({
-                controller,
-                initialValue: {
-                  values: {
-                    alignment: element.alignment,
-                    title: element.title,
-                    description: element.description,
-                    remark: element.remark,
-                    linkText: element.linkText,
-                    linkUrl: element.linkUrl,
-                  },
-                  imageItem: element.imageItem,
-                  haveLink: element.haveLink,
-                },
-                onConfirm: ({ values, imageItem, haveLink }) => {
-                  controller.updateCardElement({
-                    editor,
-                    cardValues: {
-                      alignment: values.alignment,
-                      imageItem,
-                      title: values.title,
-                      description: values.description,
-                      remark: values.remark,
-                      haveLink,
-                      linkText: values.linkText,
-                      linkUrl: values.linkUrl,
+            ? { icons: [] }
+            : {
+                enabledBgColor: true,
+                icons: [
+                  {
+                    icon: BlockLeft,
+                    onClick: () => {
+                      controller.updateCardAlignment({
+                        editor,
+                        alignment: 'leftImageRightText',
+                        path,
+                      });
                     },
-                    path,
+                    active: element.alignment === 'leftImageRightText',
+                  },
+                  {
+                    icon: BlockRight,
+                    onClick: () => {
+                      controller.updateCardAlignment({
+                        editor,
+                        alignment: 'rightImageLeftText',
+                        path,
+                      });
+                    },
+                    active: element.alignment === 'rightImageLeftText',
+                  },
+                ],
+              },
+          {
+            icons: [
+              {
+                icon: Edit,
+                onClick: () => {
+                  setCardModalConfig({
+                    controller,
+                    initialValue: {
+                      values: {
+                        alignment: element.alignment,
+                        title: element.title,
+                        description: element.description,
+                        remark: element.remark,
+                        linkText: element.linkText,
+                        linkUrl: element.linkUrl,
+                      },
+                      imageItem: element.imageItem,
+                      haveLink: element.haveLink,
+                    },
+                    onConfirm: ({ values, imageItem, haveLink }) => {
+                      controller.updateCardElement({
+                        editor,
+                        cardValues: {
+                          alignment: values.alignment,
+                          imageItem,
+                          title: values.title,
+                          description: values.description,
+                          remark: values.remark,
+                          haveLink,
+                          linkText: values.linkText,
+                          linkUrl: values.linkUrl,
+                        },
+                        path,
+                      });
+                    },
                   });
                 },
-              });
-            },
-          },
-          {
-            icon: Trash,
-            onClick: () => {
-              if (controller.confirmModal) {
-                // TODO: i18n
-                setConfirmModalConfig({
-                  title: '刪除卡片',
-                  content: '是否確認刪除此卡片？刪除後將立即移除，且此操作無法復原。',
-                  confirmText: '刪除卡片',
-                  onConfirm: () => {
+              },
+              {
+                icon: Trash,
+                onClick: () => {
+                  if (controller.confirmModal) {
+                    // TODO: i18n
+                    setConfirmModalConfig({
+                      title: '刪除卡片',
+                      content: '是否確認刪除此卡片？刪除後將立即移除，且此操作無法復原。',
+                      confirmText: '刪除卡片',
+                      onConfirm: () => {
+                        Transforms.removeNodes(editor, { at: path });
+                      },
+                    });
+                  } else {
                     Transforms.removeNodes(editor, { at: path });
-                  },
-                });
-              } else {
-                Transforms.removeNodes(editor, { at: path });
-              }
-            },
+                  }
+                },
+              },
+            ],
           },
         ]}
       />
