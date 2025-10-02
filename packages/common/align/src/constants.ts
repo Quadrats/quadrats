53f90965622a2e9189d803be7975b8969cb85743
travor20814 <travor20814@gmail.com>
2025-10-02 13:25:23 +0800

    refactor(common/align): move alignable types to constants

--- packages/common/align/src/constants.ts
+++ packages/common/align/src/constants.ts
@@ -1,1 +1,5 @@
+import { HEADING_TYPE } from '@quadrats/common/heading';
+import { PARAGRAPH_TYPE } from '@quadrats/core';
+
 export const ALIGN_TYPE = 'align';
+export const ALIGNABLE_TYPES = [PARAGRAPH_TYPE, HEADING_TYPE];
